3de38903155f2136b6b99f46d135ac51a70aae1d
Bryan Armando <mamanibryan1@gmail.com>
2023-05-09 16:09:29 -0500

    add data types

---
+++ src/type.d.ts
@@ -0,0 +1,20 @@
+export interface Language {
+  label: string,
+  color: string,
+  icon: IconType
+}
+
+export interface Project {
+  name: string
+  description: string
+  languages: Language[],
+  image: string,
+  source_code_link: string,
+  webpage_link: string
+}
+
+export interface Social {
+  name: string;
+  url: string;
+  icon: IconType;
+}
