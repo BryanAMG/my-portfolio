f044eff6630851d87e7e914ebe63f3d1ace1c3d4
Bryan Armando <mamanibryan1@gmail.com>
2023-05-09 16:37:27 -0500

    add social and tec icons

---
+++ src/components/icons/social-media/index.js
@@ -0,0 +1,11 @@
+import Github from './GitHub.astro'
+import LinkedIn from './LinkedIn.astro'
+import Twitter from './Twitter.astro'
+import Gmail from './Gmail.astro'
+
+export {
+  Github,
+  LinkedIn,
+  Twitter,
+  Gmail
+}
---
+++ src/components/icons/tecnologies/index.js
@@ -0,0 +1,45 @@
+import React from './React.astro'
+import Css from './Css.astro'
+import Html from './Html.astro'
+import Javascript from './Javascript.astro'
+import Tailwindcss from './Tailwindcss.astro'
+import Node from './Node.astro'
+import Typescript from './Typescript.astro'
+import Astro from './Astro.astro'
+import MaterialUI from './MaterialUI.astro'
+import Postman from './Postman.astro'
+import ThunderCLient from './ThunderCLient.astro'
+import Git from './Git.astro'
+import Mysql from './Mysql.astro'
+import Mongo from './Mongo.astro'
+import Query from './Query.astro'
+import Nextjs from './Nextjs.astro'
+import Vue from './Vue.astro'
+import Redux from './Redux.astro'
+import Python from './Python.astro'
+import Django from './Django.astro'
+import Express from './Express.astro'
+
+export {
+  React,
+  Css,
+  Html,
+  Javascript,
+  Tailwindcss,
+  Node,
+  Typescript,
+  Astro,
+  MaterialUI,
+  Postman,
+  ThunderCLient,
+  Git,
+  Mysql,
+  Mongo,
+  Query,
+  Nextjs,
+  Vue,
+  Redux,
+  Python,
+  Django,
+  Express
+}
