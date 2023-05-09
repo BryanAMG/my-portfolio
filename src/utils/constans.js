887ffa83a33438d0e8ce171c1e70dbce543ae821
Bryan Armando <mamanibryan1@gmail.com>
2023-05-09 16:39:28 -0500

    add icons and social constans

---
+++ src/utils/constans.js
@@ -0,0 +1,166 @@
+import {
+  React,
+  Css,
+  Javascript,
+  Tailwindcss,
+  Html,
+  Node,
+  Typescript,
+  Astro,
+  MaterialUI,
+  ThunderCLient,
+  Postman,
+  Git,
+  Mysql,
+  Mongo,
+  Query,
+  Nextjs,
+  Vue,
+  Redux,
+  Python,
+  Express
+} from "@icons/tecnologies/index";
+import { Github, LinkedIn, Twitter } from "@icons/social-media/index";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "projects",
+    title: "Proyectos",
+  },
+  {
+    id: "about",
+    title: "Sobre Mi",
+  },
+  {
+    id: "contact",
+    title: "Contactame",
+  },
+];
+
+export const socialMedia = [
+  {
+    name: 'Github',
+    url: 'https://github.com/bryan220420',
+    icon: Github
+  },
+  {
+    name: 'Linkedin',
+    url: 'https://www.linkedin.com/in/bryan-armando/',
+    icon: LinkedIn
+  },
+  {
+    name: 'Twitter',
+    url: 'https://twitter.com/Bryan22Armando',
+    icon: Twitter
+  }
+]
+
+
+export const technologiesData = {
+  react: {
+    label: 'React',
+    color: '#149eca',
+    icon: React
+  },
+  tailwindcss: {
+    label: 'TailwindCss',
+    color: '#38bdf8',
+    icon: Tailwindcss
+  },
+  javascript: {
+    label: 'JavaScript',
+    color: '#efda4d',
+    icon: Javascript
+  },
+  typescript: {
+    label: 'TypeScript',
+    color: '#3178c6',
+    icon: Typescript
+  },
+  css: {
+    label: 'CSS',
+    color: '#4189e3',
+    icon: Css
+  },
+  html: {
+    label: 'HTML',
+    color: '#E44D26',
+    icon: Html
+  },
+  node: {
+    label: 'NodeJs',
+    color: '#83CD29',
+    icon: Node
+  },
+  astro: {
+    label: 'Astro',
+    color: '#ff5d01',
+    icon: Astro
+  },
+  material: {
+    label: 'Material UI',
+    color: '#007FFF',
+    icon: MaterialUI
+  },
+  postman: {
+    label: 'Postman',
+    color: '#FF6C37',
+    icon: Postman
+  },
+  thunder: {
+    label: 'Thunder Client',
+    color: '#e33eed',
+    icon: ThunderCLient
+  },
+  git: {
+    label: 'Git',
+    color: '#f04f33',
+    icon: Git
+  },
+  mysql: {
+    label: 'Mysql',
+    color: null,
+    icon: Mysql
+  },
+  mongo: {
+    label: 'MongoDB',
+    color: '#309e40',
+    icon: Mongo
+  },
+  reactQuery: {
+    label: 'React-Query',
+    color: null,
+    icon: Query
+  },
+  next: {
+    label: 'Next js',
+    color: null,
+    icon: Nextjs
+  },
+  vue: {
+    label: 'Vue js',
+    color: null,
+    icon: Vue
+  },
+  redux: {
+    label: 'Redux',
+    color: '#764abc',
+    icon: Redux
+  },
+  python: {
+    label: 'Python',
+    color: null,
+    icon: Python
+  },
+  express: {
+    label: 'Express',
+    color: '#000',
+    icon: Express
+  },
+}
+
+
